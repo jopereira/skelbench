8266e984d6a1d27de68da47887b4ed3c422963cc
José Pereira <jop@di.uminho.pt>
2020-10-21 16:49:31 +0100

    Demonstration workload

--- src/main/java/abd/Workload.java
+++ src/main/java/abd/Workload.java
@@ -9,7 +9,11 @@ public class Workload {
     public static void populate(Random rand, Connection c) throws Exception {
         Statement s = c.createStatement();
 
-        // Criar tabelas e inserir valores
+        //---- DEMO WORKLOAD ----
+        // replace with your workload!
+        s.executeUpdate("create table demo (a int, b varchar, c int)");
+        s.executeUpdate("insert into demo values (1, 'one',0)");
+        //-----------------------
 
         s.close();
     }
@@ -17,7 +21,19 @@ public class Workload {
     public static void transaction(Random rand, Connection c) throws Exception {
         Statement s = c.createStatement();
 
-        // Executar operação
+        //---- DEMO WORKLOAD ----
+        // replace with your workload!
+        switch(rand.nextInt(2)) {
+            case 0:
+                s.executeUpdate("update demo set c=c+1 where a=1");
+                break;
+            case 1:
+                ResultSet rs = s.executeQuery("select * from demo");
+                while(rs.next())
+                    ;
+                break;
+        }
+        //-----------------------
 
         s.close();
     }
